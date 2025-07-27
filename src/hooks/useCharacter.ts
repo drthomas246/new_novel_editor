3b495567fc61046ebb0d141886b2091de36e0a9c
Yamahara Yoshihiro <yoshihiro@yamahara.email>
2025-07-28 00:35:59 +0900

    app router変更時の修正

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export function useCharacter(id: string | string[] | undefined) {
